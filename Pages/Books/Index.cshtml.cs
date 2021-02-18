e3d26e958ad68fa203771e693ee90e2cab66c006
Gorgan <alexandru.gorgan@metro.digital>
2021-02-18 23:46:31 +0200

    initial commit

---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Gorgan_Alexandru_lab8.Data;
+using Gorgan_Alexandru_lab8.Models;
+
+namespace Gorgan_Alexandru_lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Gorgan_Alexandru_lab8.Data.Gorgan_Alexandru_lab8Context _context;
+
+        public IndexModel(Gorgan_Alexandru_lab8.Data.Gorgan_Alexandru_lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
